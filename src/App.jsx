8ceab550c9f238e4a50e07249503a5c551d95fa9
NeerajQA <neeraj1264qa@gmail.com>
2025-01-04 16:05:28 +0530

    add pwa functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.js
 import React, { useState , useEffect} from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate , useLocation} from "react-router-dom";
 import Invoice from "./components/Invoice/Invoice";
 import "./App.css";
 import CustomerDetail from "./components/CustomerDetail/CustomerDetail";
@@ -8,10 +8,14 @@ import NewProduct from "./components/ProductAdded/NewProduct";
 import History from "./components/history/History";
 import { CustomerData } from "./components/data/CustomerData";
 import BluetoothPrinter from "./components/BluetoothComponent/printer";
+import AddToHomeModal from "./components/AddToHome/AddToHome";
 
 
 const App = () => {
   const [selectedProducts, setSelectedProducts] = useState([]);
+  const [installPrompt, setInstallPrompt] = useState(null);
+
+  const currentRoute = window.location.pathname;
 
    // Clear 'productsToSend' from localStorage on page reload
    useEffect(() => {
@@ -27,8 +31,49 @@ const App = () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, []);
+  const handleInstallClick = () => {
+    if (installPrompt instanceof Event) {
+      const installEvent = installPrompt;
+      installEvent.prompt();
+      installEvent.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        setInstallPrompt(null);
+      });
+    }
+  };
+
+  const handleCloseClick = () => {
+    setInstallPrompt(null);
+  };
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event) => {
+      event.preventDefault();
+      setInstallPrompt(event);
+    };
+
+    const handleClickOutsidePopup = (event) => {
+      // Check if the clicked element is not inside the install popup
+      if (!event.target.closest('.install-popup')) {
+        setInstallPrompt(null);
+      }
+    };
+
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    document.addEventListener('click', handleClickOutsidePopup);
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+      document.removeEventListener('click', handleClickOutsidePopup);
+    };
+  }, []);
 
   return (
+    <>
     <Router>
       <Routes>
       <Route path="/" element={<Navigate to="/invoice" />} />
@@ -49,6 +94,14 @@ const App = () => {
 
       </Routes>
     </Router>
+      {installPrompt && currentRoute === '/invoice' && (
+        <AddToHomeModal
+        installPrompt={installPrompt}
+        onInstallClick={handleInstallClick}
+        onCloseClick={handleCloseClick}
+        />
+      )}
+      </>
   );
 };
 
