11734d72b292906cf1c398d358778039b0727f9a
NeerajQA <neeraj1264qa@gmail.com>
2025-01-12 11:45:36 +0530

    forgot to add  backed api

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://backend-alpha-tawny.vercel.app/api'; 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = 'https://backend-alpha-tawny.vercel.app/api'; 
+// const BASE_URL = "http://localhost:5000/api";
 
 export const fetchCategories = async () => {
   const response = await fetch(`${BASE_URL}/categories`);
